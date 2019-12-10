d4a313bf4274e6bcb24940ab97bbc5813bca0251
sandeep dey <sandeep.dey@blinkhealth.com>
2019-12-09 17:17:00 -0800

    @sandeepdey adding competition pricing data and other sql

---
+++ competition.sql
@@ -0,0 +1,110 @@
+-- select * from mktg_dev.sdey_magic_card_pricing_top_300_20191121;
+-- select count(*) from mktg_dev.sdey_magic_card_pricing_top_300_20191121;
+-- drop table mktg_dev.sdey_magic_card_pricing_top_300_20191121;
+-- CREATE TABLE mktg_dev.sdey_magic_card_pricing_top_300_20191121 AS
+-- SELECT
+-- 	med_price.gcn,
+-- 	gcnseqno,
+-- 	description,
+-- 	med_price.medid,
+-- 	pharmacy_network_id,
+-- 	started_on,
+-- 	branded,
+-- 	med_price.unit_price as unit_price_sell,
+-- 	network_pricing_mac.unit_price as unit_price_mac,
+-- 	unit_margin,
+-- 	dispensing_fee_margin,
+-- 	dispensing_fee_margin::float + 1.75 AS fixed_price,
+-- 	brandmedname,
+-- 	hicldesc,
+-- 	gtcdesc,
+-- 	stcdesc,
+-- 	top_300_drugs_20191121_1.strength,
+-- 	form
+-- FROM
+-- 	transactional.med_price
+-- 	INNER JOIN mktg_dev.top_300_drugs_20191121_1 ON med_price.gcn = top_300_drugs_20191121_1.gcn
+-- 	INNER JOIN transactional.med ON med_price.medid = med.medid
+-- 	INNER JOIN drugs_etl.network_pricing_mac ON network_pricing_mac.gcn_seqno = med.gcn_seqno
+-- WHERE
+-- 	pharmacy_network_id = 4 -- 	OR pharmacy_network_id = 5
+-- 	AND ended_on IS NULL
+-- 	AND end_date is NULL
+-- 	AND mac_list = 'BLINK02';
+
+
+-- drop table mktg_dev.sdey_magic_card_pricing_and_competitive_data_top_300_20191122;
+
+-- CREATE TABLE mktg_dev.sdey_magic_card_pricing_and_competitive_data_top_300_20191122 as
+-- SELECT
+-- 	sdey_magic_card_pricing_top_300_20191121.gcn,
+-- 	gcnseqno,
+-- 	description,
+-- 	medid,
+-- 	pharmacy_network_id,
+-- 	started_on,
+-- 	branded,
+-- 	unit_price_sell,
+-- 	unit_price_mac,
+-- 	unit_margin,
+-- 	dispensing_fee_margin,
+-- 	fixed_price,
+-- 	brandmedname,
+-- 	hicldesc,
+-- 	gtcdesc,
+-- 	stcdesc,
+-- 	strength,
+-- 	form,
+-- 	quantity,
+-- 	all_cnt,
+-- 	cpr_pct_00_price,
+-- 	cpr_pct_25_price,
+-- 	cpr_pct_50_price,
+-- 	cpr_pct_75_price,
+-- 	cpr_pct_100_price,
+-- 	geo_cnt,
+-- 	cpr_geo_pct_00_price,
+-- 	cpr_geo_pct_25_price,
+-- 	cpr_geo_pct_50_price,
+-- 	cpr_geo_pct_75_price,
+-- 	cpr_geo_pct_100_price,
+-- 	lowest_price_pharmacy,
+-- 	brookshire_price,
+-- 	filled_orders as filled_orders_2019
+-- FROM
+-- 	mktg_dev.sdey_magic_card_pricing_top_300_20191121
+-- LEFT OUTER JOIN
+-- 	mktg_dev.sdey_competitve_pricing_top_300_20191121
+-- ON
+-- 	sdey_magic_card_pricing_top_300_20191121.gcn = sdey_competitve_pricing_top_300_20191121.gcn
+-- LEFT OUTER JOIN
+-- 	mktg_dev.sdey_filled_orders_2019_gcn
+-- ON
+-- 	sdey_magic_card_pricing_top_300_20191121.gcnseqno = sdey_filled_orders_2019_gcn.gcn_seqno
+
+-- select cpr_geo_pct_00_price from mktg_dev.sdey_magic_card_pricing_and_competitive_data_top_300_20191122
+
+
+-- Magic Card Fills SQL
+-- SELECT
+-- 	gcn_seqno,
+-- 	count(*) AS fills
+-- FROM
+-- 	dwh.fact_order_item foi
+-- 	JOIN dwh.dim_user du ON du.dw_user_id = foi.dw_user_id
+-- 		AND foi.account_id = du.account_id
+-- WHERE
+-- 	foi.fill_sequence IS NOT NULL
+-- 	AND foi.last_pbm_adjudication_timestamp_approved > '2019-08-01'
+-- 	AND du.is_internal = FALSE
+-- 	AND du.is_phantom = FALSE
+-- 	AND foi.is_fraud = FALSE
+-- 	AND foi.pharmacy_network_id = 4
+-- GROUP BY
+-- 	1
+
+
+-- select *
+-- from transactional.med_price
+-- where ended_on is null -- and gcn = 64324
+-- order by gcn
---
+++ etc.sql
@@ -0,0 +1,8 @@
+-- select * from pg_tables where tablename like '%top_300_drugs%'
+
+-- drop table mktg_dev.top_300_drugs_20191121
+
+-- GRANT SELECT ON mktg_dev.sdey_magic_card_pricing_and_competitive_data_top_300_20191122 TO "public";
+
+
+-- select * from fifo.bsd_retirement_input order by fill_date desc;
